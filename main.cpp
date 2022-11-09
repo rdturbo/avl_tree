df934089fb66e918689a7819e0ae8644555f3e32
rdturbo <rupayan98@gmail.com>
2022-11-09 14:26:03 -0500

    figured input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main()
+void readFile(const std::string &file_name)
 {
-    std::cout << "Hello World";
+    std::ifstream file{file_name};
+    std::vector<std::string> commands;
+
+    std::string input;
+    while (file >> input)
+    {
+        commands.push_back(input);
+    }
+
+    for (std::string command : commands)
+    {
+        std::cout << command << std::endl;
+    }
+}
+
+int main(int n, char *args[])
+{
+    const std::string kFileName = args[1];
+    readFile(kFileName);
 
     return 0;
 }
